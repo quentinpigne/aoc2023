c08f8c984fba0a1e3daa5313e263a66d992ef9c0
Quentin Pigné <quentin.pigne@gmail.com>
2023-12-04 06:57:43 +0000

    implementing day 3 part 2

--- days/day3.go
+++ days/day3.go
@@ -32,6 +32,41 @@ func ComputeDay3Part1(fileLines []string) int {
 
 func ComputeDay3Part2(fileLines []string) int {
 	count := 0
+	for i := 0; i < len(fileLines); i++ {
+		for _, j := range findAllGears(fileLines[i]) {
+			gearRatio := 1
+			numberOfMatch := 0
+			frame := getFrame(fileLines[i], j)
+			if i > 0 {
+				for _, k := range findAllNumbers(fileLines[i-1]) {
+					value, _ := strconv.Atoi(fileLines[i-1][k[0]:k[1]])
+					if numberInFrame(k, frame) {
+						gearRatio *= value
+						numberOfMatch += 1
+					}
+				}
+			}
+			for _, k := range findAllNumbers(fileLines[i]) {
+				value, _ := strconv.Atoi(fileLines[i][k[0]:k[1]])
+				if numberInFrame(k, frame) {
+					gearRatio *= value
+					numberOfMatch += 1
+				}
+			}
+			if i < len(fileLines)-1 {
+				for _, k := range findAllNumbers(fileLines[i+1]) {
+					value, _ := strconv.Atoi(fileLines[i+1][k[0]:k[1]])
+					if numberInFrame(k, frame) {
+						gearRatio *= value
+						numberOfMatch += 1
+					}
+				}
+			}
+			if numberOfMatch == 2 {
+				count += gearRatio
+			}
+		}
+	}
 	return count
 }
 
@@ -39,6 +74,11 @@ func ComputeDay3Part2(fileLines []string) int {
 
 func findAllNumbers(sequence string) [][]int {
 	re := regexp.MustCompile("(\\d+)")
+	return re.FindAllStringIndex(sequence, -1)
+}
+
+func findAllGears(sequence string) [][]int {
+	re := regexp.MustCompile("([*])")
 	return re.FindAllStringIndex(sequence, -1)
 }
 
@@ -54,7 +94,16 @@ func getFrame(line string, index []int) []int {
 	return []int{startPos, endPos}
 }
 
+func numberInFrame(k []int, frame []int) bool {
+	return (frame[0] <= k[0] && frame[1]-1 >= k[0]) || (frame[0] <= k[1]-1 && frame[1]-1 >= k[1]-1)
+}
+
 func containsSymbol(sequence string) bool {
 	re := regexp.MustCompile("[^.\\d]")
+	return re.MatchString(sequence)
+}
+
+func containsDigit(sequence string) bool {
+	re := regexp.MustCompile("\\d")
 	return re.MatchString(sequence)
 }
